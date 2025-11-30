9e08c5a9ca92817abfd0b80ffd19a20d54682160
Eduardo Barbosa Rodrigues <eduardo.rodrigues@modalgr.io>
2025-11-30 19:59:45 -0300

    tp01

---
+++ TPs/CBTSWE2_TP01/Interface/IBookRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TP01.Models;
+
+// Eduardo Barbosa Rodrigues e Vinicius Pontes Oliva
+// Matheus Leandro Terra Luciano (CB3024881)
+
+namespace TP01.Interface
+{
+    public interface IBookRepository
+    {
+        ICollection<Book> getAll();
+        void add(Book book);
+    }
+}
---
+++ TPs/CBTSWE2_TP01/Model/Author.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// Eduardo Barbosa Rodrigues e Vinicius Pontes Oliva
+// Matheus Leandro Terra Luciano (CB3024881)
+
+namespace TP01.Models
+{
+    public class Author
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public char Gender { get; set; }
+
+        public Author(string name, string email, char gender)
+        {
+            this.Name = name;
+            this.Email = email;
+            this.Gender = gender;
+        }
+
+        public override string ToString()
+        {
+            return $"Author[name={this.Name}, email={this.Email}, gender={this.Gender}]";
+        }
+    }
+}
