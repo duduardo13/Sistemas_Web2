b86aa356d181fec310a4378a19f1aa06b157c1c0
Eduardo Barbosa Rodrigues <eduardo.rodrigues@modalgr.io>
2025-12-02 16:05:28 -0300

    tp03

---
+++ TPs/CBTSWE2_TP03/Repositories/IProdutoRepository.cs
@@ -0,0 +1,15 @@
+﻿using CBTSWE2_TP03.Models;
+
+//Feito por Eduardo Barbosa Rodrigues e Vinicius Pontes Oliva
+
+namespace CBTSWE2_TP03.Repositories
+{
+    public interface IProdutoRepository
+    {
+        Task<IEnumerable<Produto>> GetProdutos();
+        Task<Produto> GetProdutoById(int id);
+        Task AddProduto(Produto produto);
+        Task UpdateProduto(Produto produto);
+        Task DeleteProduto(int id);
+    }
+}
